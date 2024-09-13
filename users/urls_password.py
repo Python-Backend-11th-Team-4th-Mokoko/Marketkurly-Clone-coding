aa815c3dcc02834638b07330fdf99cbd153b8219
MinjunSong <minjunevmf2@gmail.com>
2024-09-13 15:59:11 +0900

    비밀번호 초기화 기능 수정
    이메일인증 정상적으로 됨

--- users/urls_password.py
+++ users/urls_password.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy
 
+app_name = "password"
 
 urlpatterns = [
     # 비밀번호 재설정 URL 패턴
     path('password-reset/', 
         auth_views.PasswordResetView.as_view(email_template_name='users/password_reset_email.html',
-        subject_template_name='users/password_reset_subject.txt',template_name='users/password_reset.html'), 
+        subject_template_name='users/password_reset_subject.txt', success_url = reverse_lazy('password:password_reset_done'), template_name='users/password_reset.html'), 
         name='password_reset'),
     
     path('password-reset/done/', 
@@ -14,7 +16,7 @@ urlpatterns = [
         name='password_reset_done'),
     
     path('reset/<uidb64>/<token>/', 
-        auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), 
+        auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html', success_url = reverse_lazy("password:password_reset_complete")), 
         name='password_reset_confirm'),
     
     path('reset/done/', 
